680a6cb27db6e6c94c4a82767a475ce08f7c4a1f
Jeffrey Yu <jwy5140@gmail.com>
2019-03-07 23:36:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,12 +24,11 @@ def apply_coupons(cart, coupons)
       if cart[x][:count] <= coupons[x][:num]
         cart["#{x} W/COUPON"] = {price: coupons[x][:cost], clearance: cart[x][:clearance], count: (cart[x][:count]/coupons[x][:num]).floor}
         cart[x][:count] = cart[x][:count]%coupons[x][:num]
-        if cart[x][:count] === 0 
-          cart.delete! 
       end
     end
   }
-  cart 
+  cart.delete_if! {|x,y|
+    
 end
 
 def apply_clearance(cart)
